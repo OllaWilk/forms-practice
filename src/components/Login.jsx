4a2bcb635c8ed130a23d0b2f1458b9e6e9ca90e6
OllaWilk <alex.dev.wilk@gmail.com>
2025-11-28 13:39:41 +0100

    add basic Login form with state handling for email and password

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,42 @@
+import { useState } from "react";
+
 export default function Login() {
-  const handleSubmit = () => {
+  const [data, setData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('submited')
+    console.log('submited', data)
   }
+
+  const handleChange= (e) => {
+    const {name, value} = e.target;
+
+    setData((prev) => ({
+      ...prev,
+      [name]: value
+    }))
+  }
+
+  const handleReset = () => {
+    setData({ email: "", password: "" });
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} onReset={handleReset}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" value={data.email} onChange={handleChange} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" value={data.password} onChange={handleChange}/>
         </div>
       </div>
 
